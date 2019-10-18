2d7dcdb81374a42ec0d9b77888600f7381e5ccd5
Antonio Matheus <antonimathsilva@gmail.com>
2019-10-18 10:23:07 -0300

    @feat: bottom tab included and styled

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 export default isSigned =>
   createAppContainer(
@@ -14,9 +15,23 @@ export default isSigned =>
           SignIn,
           SignUp,
         }),
-        App: createBottomTabNavigator({
-          Dashboard,
-        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            Profile,
+          },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#fff',
+              inactiveTintColor: 'rgba(255, 255, 255, 0.7)',
+              style: {
+                backgroundColor: '#2B1A2F',
+                borderTopColor: '#2B1A2F',
+              },
+            },
+          }
+        ),
       },
       {
         initialRouteName: isSigned ? 'App' : 'Sign',
