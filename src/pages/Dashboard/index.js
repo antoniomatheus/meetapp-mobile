80408163c4223431ca2cd5f55263e743990880f5
Antonio Matheus <antonimathsilva@gmail.com>
2019-10-18 10:04:56 -0300

    @feat: routing based on auth

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Container } from './styles';
+
+export default function Dashboard() {
+  return (
+    <Container>
+      <Text>Dashboard</Text>
+    </Container>
+  );
+}
